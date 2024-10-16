d2c993d8ac561eca343195a907cc4b6a33bdcbcf
junx21 <jxuche@myuax.com>
2024-10-16 21:59:55 +0200

    Ej 3

---
+++ proyectos3,4,5/main.cpp
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <iostream>
+#include <fmt/core.h>
+
+int main () {
+    std::cout << "Hola Mundo" << std::endl;
+
+    fmt::print("Prueba con fmt\n");
+
+    return 0;
+}
---
+++ proyectos3,4,5/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
