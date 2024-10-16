f4e3721c2206f507df588c0a1d8a7d5d60cc93bf
junx21 <jxuche@myuax.com>
2024-10-16 21:49:33 +0200

    Ejercicio 2

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <iostream>
+#include "Estudiante.h"
 
 int g = 20;
 
@@ -15,6 +16,21 @@ int main() {
     std::cout << "\nValor de g antes de la función suma: " << g << std::endl;
     std::cout << "La suma es: " << suma(5, g) << std::endl;
     std::cout << "Valor de g después de la función suma: " << g << std::endl;
+
+    Persona JP;
+    JP.setNombre("Manolo");
+    JP.setEdad(68);
+    JP.setGenero("Masculino");
+
+
+    Estudiante st;
+    st.setNombre("Pedro");
+    st.setEdad(22);
+    st.setGenero("Masculino");
+    st.setGrado("Ing. Informatica");
+
+    std::cout << "Detalles del estudiante st:" << std::endl << std::endl;
+    st.mostrarDetalles();
 
     return 0;
 }
