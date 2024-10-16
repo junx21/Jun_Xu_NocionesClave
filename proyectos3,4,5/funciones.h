e4c8b4d2131acf4d16a9b51e6f089b3df2cdeb5c
junx21 <jxuche@myuax.com>
2024-10-16 22:05:37 +0200

    Ej 4

---
+++ proyectos3,4,5/funciones.h
@@ -0,0 +1,10 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+
+int suma(int a, int b);
+int resta(int a, int b);
+
+
+
+#endif
---
+++ proyectos3,4,5/matematicas.h
@@ -0,0 +1,12 @@
+#ifndef MATEMATICAS_H
+#define MATEMATICAS_H
+
+namespace matematicas {
+    int multiplicacion(int a, int b);
+    int division(int a, int b);
+}
+
+
+
+#endif
+
